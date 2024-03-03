ef4302821c9cfdcdd02468a7609f55f6ab5711e7
Felipe Clarindo <fglpc2@gmail.comgit config --global user.email fglpc2@gmail.com>
2024-03-03 20:03:59 -0300

    Implementação match case para melhor funcionamento e organização do codigo e melhorando a experiencia do usuario

--- main.py
+++ main.py
@@ -1,29 +1,63 @@
 from functions import *
+from time import sleep
 
 
 banner()
-input("APERTE ENTER PARA CONTINUAR")
+input("APERTE ENTER PARA VERIFICAÇÃO ")
 
-user = str(input("User> "))
-passw = str(input("Password> "))
-try:
-    login = fazer_login(user, passw)
+while True: 
+    banner()
+    user = str(input("User> "))
+    passw = str(input("Password> "))
 
-    if login == "Logado!":
-        print("[1] Criptografar")
-        print("[2] Desencriptografar")
-        print("[3] Avisos")
-        choice = int(input("Digite o desejado: "))
-        if choice == 1:
-            palavra = str(input("Digite a palavra ou texto que deseja criptografar!"))
+    try:
+        print("Fazendo login...")
+        sleep(1)
+        login = fazer_login(user, passw)
 
-        elif choice == 2:
-            str(input("Digite a palavra ou texto que deseja desincriptografar em base nossa chave padrão."))
-        elif choice == 3:
-            print("Usamos nossa chave de criptografia, caso deseja usar outra troque-a. ")
+        if login == True:
+            
+            while True:
+                banner()
+                print("[1] Criptografar")
+                print("[2] Desincriptografar")
+                print("[3] Avisos")
+                print("[4] Finalizar")
+                choice = int(input("Digite o desejado: "))
+                match choice:
+                    case 1:
+                        texto = str(input("Digite o que deseja criptografar: "))
+                        texto_criptografado = criptografar(texto)
+                        banner()
+                        print(f"{texto} Criptografado:  {texto_criptografado}")
+                        input("APERTE ENTER PARA CONTINUAR")
+                        
+                    case 2:
+                        texto = str(input("Em base nossa chave padrão \nDigite o que deseja desincriptografar: "))
+                        texto_desencriptografado = desincriptografar(texto)
+                        banner()
+                        print(f"{texto} Desencriptografado: {texto_desencriptografado}")
+                        input("APERTE ENTER PARA CONTINUAR")
+                        
+                    case 3:
+                        banner()
+                        print("Usamos nossa chave de criptografia, caso deseja usar outra troque-a. ")
+                        print("Desenvolvido por: Felipe Clarindo")
+                        input("APERTE ENTER PARA CONTINUAR")
+                    case 4:
+                        banner()
+                        print("Finalizado")
+                        break
+                    case _:
+                        banner()
+                        print("Opção Invalida!")
+                        input("APERTE ENTER PARA CONTINUAR")
+            break
 
-    else:
-        print("Acesso restrito!")
+        else:
+            banner()
+            print("Acesso restrito!")
+            input("APERTE ENTER PARA CONTINUAR")
 
-except Exception as e:
-    print(f"Erro: {e}")
+    except Exception as e:
+        print(f"Erro: {e}")
